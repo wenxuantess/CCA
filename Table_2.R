f5cf02bcd939bf4c044e05197e66f16df338660d
Wenxuan Li <wenxuan.tess@gmail.com>
2016-06-21 11:22:32 -0400

    Update Table_2.R

--- Table_2.R
+++ Table_2.R
@@ -1,10 +1,6 @@
 dat <- read.csv("~/Desktop/CCA/R/Index Value.csv", sep=";")
 
 library("ggplot2")
-
-ggplot(data=dat, aes(x=X, y=Farm.Economy.Barometer, group=1)) + geom_line(colour="black", size=1.5) + geom_point(colour="black", size=4, shape=21)
-
-install.packages("png")
 library(png)
 library(grid)
 
@@ -20,22 +16,28 @@ Index.Value$Time <- factor(Index.Value$Time, levels = as.character(Index.Value$T
 ggplot(data=Index.Value, aes(x=Time, y=Farm.Economy.Barometer, group=1, label=Farm.Economy.Barometer)) +
   annotation_custom(g, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)+ 
   # Add the title
-  ggtitle("Thinking about large farm investments") + 
+  ggtitle("Ag Economy Barometer") + 
   # Add the grid
-  theme(plot.title = element_text(lineheight=2, face="bold"),
+  theme(plot.title = element_text(lineheight=4, face="bold",size=20),
     panel.ontop=TRUE, panel.background = element_rect(colour = NA, fill="transparent"),
     panel.grid.major.x = element_blank(),
     panel.grid.major.y = element_line(color = "lightgrey", size = 0.5),
     panel.grid.minor.y = element_line(color = "lightgrey", size = 0.5)) +
   # Format the line
-  geom_line(colour="black", size=1.5, stat = "identity",position = "identity") +
+  geom_line(colour="wheat4", size=1.5, stat = "identity",position = "identity") +
   # Format the points
   geom_point(colour="black", size=0.5, shape=1, stroke=2) +
   geom_text(vjust = 0.5, hjust = -0.5) +
+  #Label the axis
+  labs(x="Time", y="Farm Economy Barometer") +
   # Adjust the chart size and proportion
   coord_fixed(ratio = 0.1676) + 
-  annotation_custom(grob = textGrob("Producer Survey"),xmin = 2, xmax = 4, ymin = -Inf, ymax = 0)
+  annotate("text",x = 7, y = 112, label="Producer Survey", size=6) 
   
+
+
+
+
 # Graph 2 Current and Future
 # Call the watermark
 img <- readPNG("Desktop/_Final-watermark.png")
@@ -48,24 +50,26 @@ Index.Value$Time <- factor(Index.Value$Time, levels = as.character(Index.Value$T
 ggplot(data=Index.Value, aes(x=Time, group=2)) +
   annotation_custom(g, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf)+ 
   # Add the title
-  ggtitle("Thinking about large farm investments") + 
+  ggtitle("Current and Future") + 
   # Add the grid
-  theme(plot.title = element_text(lineheight=2, face="bold"),
+  theme(plot.title = element_text(lineheight=4, face="bold",size=20),
   panel.ontop=TRUE, panel.background = element_rect(colour = NA,fill="transparent"),
   panel.grid.major.x = element_blank(),
   panel.grid.major.y = element_line(color = "lightgrey", size = 0.5),
   panel.grid.minor.y = element_line(color = "lightgrey", size = 0.5)) + 
   # Format the lines
-  geom_line(aes(y = Index.of.Current.Conditions), colour = "black") +
-  geom_line(aes(y = Index.of.Future.Expectations), colour = "blue") +
+  geom_line(aes(y = Index.of.Current.Conditions), colour = "peru", size=1.5) +
+  geom_line(aes(y = Index.of.Future.Expectations), colour = "mediumturquoise", size=1.5) +
   # Format the points
   geom_point(aes(y = Index.of.Current.Conditions),colour="black", size=0.5, shape=1, stroke=2) + 
-  geom_point(aes(y = Index.of.Future.Expectations),colour="blue", size=0.5, shape=1, stroke=2) +
+  geom_point(aes(y = Index.of.Future.Expectations),colour="black", size=0.5, shape=1, stroke=2) +
   geom_text(aes(y = Index.of.Current.Conditions, label=Index.of.Current.Conditions), vjust = 0.5, hjust = -0.5) + 
   geom_text(aes(y = Index.of.Future.Expectations, label=Index.of.Future.Expectations), vjust = 0.5, hjust = -0.5) + 
+  #Label the axis
+  labs(x="Time", y= "Index Values") +
   #  Adjust the chart size and proportion
   coord_fixed(ratio = 0.1) +
-  annotation_custom(grob = textGrob("Producer Survey"),xmin = 2, xmax = 2, ymin = -4.5, ymax = -4.55)
+  annotate("text",x = 7, y = 117, label="Producer Survey", size=6)
 
 
 # Graph 3
